72f8ed2aff0b45a1d7e073f7640741383952e696
jhonel lupaca <300lupaca@gmail.com>
2022-10-12 23:44:27 -0500

    creado contenido

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,8 +34,29 @@ class _HomeState extends State<Home> {
               tabs: [Tab(text: "facebook"), Text("twiter"), Text("instagrama")],
             ),
           ),
-          body: TabBarView(
-              children: [Text("hola1"), Text("hola2"), Text("hola3")]),
+          body: TabBarView(children: [
+            Column(
+              children: [
+                Text(lorem(paragraphs: 2, words: 60)),
+                Container(
+                  width: 500,
+                  height: 500,
+                  child: Image.asset('imagenes/persona1.jpeg'),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text(lorem(paragraphs: 2, words: 60)),
+                Container(
+                  width: 500,
+                  height: 500,
+                  child: Image.asset('imagenes/persona2.jpeg'),
+                )
+              ],
+            ),
+            Text("hola3")
+          ]),
         ));
   }
 }
